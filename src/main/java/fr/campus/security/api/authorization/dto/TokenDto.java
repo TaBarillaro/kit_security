213ec695aca0756db43ec1ffb3b3cb0532bcf787
Tania BARILLARO <tania.barillaro@Digital-Grenoble.local>
2025-03-03 11:55:06 +0100

    create records

---
+++ src/main/java/fr/campus/security/api/authorization/dto/TokenDto.java
@@ -0,0 +1,4 @@
+package fr.campus.security.api.authorization.dto;
+
+public record TokenDto(String token, String username) {
+}
---
+++ src/main/java/fr/campus/security/api/authorization/dto/UserCredentialsDto.java
@@ -0,0 +1,4 @@
+package fr.campus.security.api.authorization.dto;
+
+public record UserCredentialsDto(String username, String password) {
+}
