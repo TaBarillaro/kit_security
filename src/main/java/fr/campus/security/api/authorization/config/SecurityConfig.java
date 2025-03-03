c7048223bf9dc1b60d7979150cdd9efc99bffbcf
Tania BARILLARO <tania.barillaro@Digital-Grenoble.local>
2025-03-03 13:49:04 +0100

    edit securityConfig to test users

--- src/main/java/fr/campus/security/api/authorization/config/SecurityConfig.java
+++ src/main/java/fr/campus/security/api/authorization/config/SecurityConfig.java
@@ -1,10 +1,20 @@
 package fr.campus.security.api.authorization.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -20,5 +30,52 @@ public class SecurityConfig {
                         .anyRequest().authenticated()
                 )
                 .build();
+    }
+
+    private final AuthenticationConfiguration authenticationConfiguration;
+
+    private final UserDetailsService userDetailsService;
+
+    public SecurityConfig(AuthenticationConfiguration authenticationConfiguration, UserDetailsService userDetailsService) {
+        this.authenticationConfiguration = authenticationConfiguration;
+        this.userDetailsService = userDetailsService;
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(userDetailsService);
+        authProvider.setPasswordEncoder(passwordEncoder());
+        return authProvider;
+    }
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        // return new BCryptPasswordEncoder();
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+    @Bean
+    public AuthenticationManager authenticationManager() throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    // Fake users in memory
+    @Bean
+    public static UserDetailsService userDetailsService() {
+        InMemoryUserDetailsManager userDetailsManager = new InMemoryUserDetailsManager();
+        userDetailsManager.createUser(
+                User.withDefaultPasswordEncoder()
+                        .username("admin")
+                        .password("admin123")
+                        .roles("ADMIN")
+                        .build()
+        );
+        userDetailsManager.createUser(
+                User.withDefaultPasswordEncoder()
+                        .username("user")
+                        .password("user123")
+                        .roles("USER")
+                        .build()
+        );
+        return userDetailsManager;
     }
 }
