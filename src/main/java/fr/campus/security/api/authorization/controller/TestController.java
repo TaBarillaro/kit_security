8fe9e9039d499d65ac04e7a991297040da7dde84
Tania BARILLARO <tania.barillaro@Digital-Grenoble.local>
2025-03-03 14:07:20 +0100

    edit testController

--- src/main/java/fr/campus/security/api/authorization/controller/TestController.java
+++ src/main/java/fr/campus/security/api/authorization/controller/TestController.java
@@ -1,5 +1,6 @@
 package fr.campus.security.api.authorization.controller;
 
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,8 +19,15 @@ public class TestController {
         return "user";
     }
 
+    @Secured("ROLE_ADMIN")
     @GetMapping("/admin")
-    public String doAdmin() {
+    public String doAdmin(){
         return "admin";
+    }
+
+    @Secured("ROLE_USER")
+    @GetMapping("/user/verified")
+    public String doUserVerified(){
+        return "verified";
     }
 }
